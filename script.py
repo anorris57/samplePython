41c2f17b1ac4e292128ae5f94abd270dfe807ba1
Angela Norris <cocoanorris@yahoo.com>
2019-09-15 16:17:38 -0400

    made some changes

--- script.py
+++ script.py
@@ -5,15 +5,16 @@ print(sys.version)
 print(sys.executable)
 
 
-def greet(who_to_greet):
+""" def greet(who_to_greet):
     greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+    return greeting """
 
 #print(greet('World'))
 #print(greet('Angela'))
 
 r = requests.get('https://coreyms.com')
 print(r.status_code)
+print(r.ok)
 
 """ name = input ("Your name? ")
 print("Hello,", name) """
